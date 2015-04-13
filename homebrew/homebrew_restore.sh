b42c15c00aa402ecfa1a42e6945860a138cda675
Eriks Vitolins <evitolins@gmail.com>
2015-04-13 23:40:25 +0900

    Added homebrew install logic to homebrew_restore.sh

--- homebrew/homebrew_restore.sh
+++ homebrew/homebrew_restore.sh
@@ -2,6 +2,13 @@
 
 # This file restores homebrew packages from generated backup files
 
+if ! command -v brew >/dev/null; then
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+     echo "Homebrew already installed"
+fi
+
 cat tap.txt | xargs brew tap
 cat list.txt | xargs brew install
 cat casklist.txt | xargs brew cask install
